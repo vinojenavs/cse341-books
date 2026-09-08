2aad1a078bde42b8623abade365958ff4f9d1666
vinojenavs <oajenavi@bupathway.edu>
2026-09-08 14:48:43 +0100

    W01 activity

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
+
 import app from "./app.js";
+import { connectToDb } from "./src/db/connect.js";
 
 const PORT = process.env.PORT;
 
-if(!PORT) {
+if (!PORT) {
     throw new Error('Port is not defined.')
-}
+};
 
-app.listen(PORT, () => {
-    console.log(`Server listening at 127.0.0.1:${PORT}`);
-})
+const startServer = async () => {
+    try {
+        await connectToDb();
+        app.listen(PORT, () => {
+            console.log(`Server listening at 127.0.0.1:${PORT}`);
+        });
+    } catch (error){
+        console.error('Database connection failed:', error.message);
+        process.exit(1);
+    };
+};
+
+await startServer();
+
